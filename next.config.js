cf52b6708e6ad1a5c1977e3f303c0eb8052cc4ee
M0hc3n <mouhcen.chouireb@ensia.edu.dz>
2024-12-29 00:23:21 +0100

    added mdx setup

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
-import createMDX from "@next/mdx";
+import * as NextMdx from "@next/mdx";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
-const withMDX = createMDX({});
+const withMDX = NextMdx.default({
+  extension: /\.mdx?$/,
+});
 
 export default withMDX(nextConfig);
