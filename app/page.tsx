034fa12ca02957c8c303815de3cc1b4fc56563de
M0hc3n <mouhcen.chouireb@ensia.edu.dz>
2025-07-25 19:16:15 +0100

    removed blogs

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           <AboutMe />
           {/* <Projects /> */}
           <Interests />
-          <Blogs />
+          {/* <Blogs /> */}
           <div className="px-[30px] w-full lg:w-[1000px] lg:px-0 mx-auto">
           <Footer />
         </div>
